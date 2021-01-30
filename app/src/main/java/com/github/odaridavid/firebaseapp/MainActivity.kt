8174b0043d1b67e522d3c511c105ac674b647dd5
David Odari <davidkibzodari@gmail.com>
2021-01-30 13:14:45 +0100

    Create Ui to be used

--- app/src/main/java/com/github/odaridavid/firebaseapp/MainActivity.kt
+++ app/src/main/java/com/github/odaridavid/firebaseapp/MainActivity.kt
@@ -1,5 +1,18 @@
 package com.github.odaridavid.firebaseapp
 
+import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private val selectedImageImageView: ImageView by lazy {
+        findViewById(R.id.selected_image_image_view)
+    }
+
+    fun uploadImage(view: View) {
+         // TODO Select an image
+        //  TODO Upload Image
+    }
+
+}
