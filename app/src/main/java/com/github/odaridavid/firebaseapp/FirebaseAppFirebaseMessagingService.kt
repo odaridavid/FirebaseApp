ef9efc42b819770f08a8c0801e3b4b14e9b8d8fa
David Odari <davidkibzodari@gmail.com>
2021-01-30 15:46:48 +0100

    Add firebase cloud messaging service

---
+++ app/src/main/java/com/github/odaridavid/firebaseapp/FirebaseAppFirebaseMessagingService.kt
@@ -0,0 +1,11 @@
+package com.github.odaridavid.firebaseapp
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class FirebaseAppFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+        //TODO
+    }
+}
